51da8f8c26a09033905e1aecff16c0bbf300812c
Rogerio A Serapiao <rserapa@gmail.com>
2022-02-05 20:44:42 -0300

    adicionado validações do formulário

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -84,12 +84,27 @@ function clearForm() {
   setFormError("cep", "");
   setFormError("number", "");
 
+  state.address = new Address();
+
   state.inputCep.focus();
 }
 
-async function handleBtnSaveClick(event) {
-  event.preventDefault();  
-  listController.addCard(state.address);
+function handleBtnSaveClick(event) {
+  event.preventDefault(); 
+  
+  const errors = addressService.getErrors(state.address);
+  const keys = Object.keys(errors);
+  
+  if (keys.length > 0) {
+    keys.forEach( key => {
+      setFormError(key, errors[key]);
+    });
+  }
+  else {
+    listController.addCard(state.address);
+    clearForm();
+  }
+  
 }
 
 function setFormError(key, value) {
