3b49b6807ef851c843e43ea15a8058300419a6b5
Rogerio A Serapiao <rserapa@gmail.com>
2022-02-06 12:31:18 -0300

    inserção de modal para contato

--- js/dsviacep.js
+++ js/dsviacep.js
@@ -1,5 +1,9 @@
 import * as formController from './controllers/form-controller.js';
 import * as listController from './controllers/list-controller.js';
+import * as pageController from './controllers/page-controller.js';
+import * as modalController from './controllers/modal-controller.js';
 
 formController.init();
 listController.init();
+pageController.init();
+modalController.init();
